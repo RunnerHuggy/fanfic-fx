341eed0f1ae9cfadeb14579802bc647c8dcbd7fc
Runner <romain.mourouvin2004@gmail.com>
2026-06-24 20:46:03 +0200

    Created the Fandom and the FandomType class.

---
+++ src/main/java/runner/huggy/fanficfx/objects/FandomType.java
@@ -0,0 +1,26 @@
+package runner.huggy.fanficfx.objects;
+
+public enum FandomType {
+    MOVIE,
+    VIDEO_GAME,
+    TOYS,
+    TV_SERIES,
+    BOOK;
+
+    @org.jetbrains.annotations.NotNull
+    @org.jetbrains.annotations.Contract(pure = true)
+    public String getType(){
+        if (this == FandomType.MOVIE) {
+            return "Movie";
+        } else if (this == FandomType.VIDEO_GAME) {
+            return "Video Game";
+        } else if (this == FandomType.TOYS){
+            return "Toys";
+        } else if (this == FandomType.TV_SERIES){
+            return "TV Series";
+        } else if (this == FandomType.BOOK){
+            return "Book";
+        }
+        return "";
+    }
+}
